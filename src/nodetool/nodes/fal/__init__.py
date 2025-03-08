086892908300b5a5682b9eb82af4f1e3a44403ff
Matthias Georgi <matti.georgi@gmail.com>
2025-03-08 11:50:28 +0100

    init

---
+++ src/nodetool/nodes/fal/__init__.py
@@ -0,0 +1,6 @@
+import nodetool.nodes.fal.llm
+import nodetool.nodes.fal.text_to_image
+import nodetool.nodes.fal.text_to_audio
+import nodetool.nodes.fal.speech_to_text
+import nodetool.nodes.fal.image_to_image
+import nodetool.nodes.fal.image_to_video
